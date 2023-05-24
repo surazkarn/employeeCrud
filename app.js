a19efd6c456013986466b5572781cb41b276b558
surazkarn <surazkarn8848@gmail.com>
2023-05-24 14:18:18 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const employeeRoutes = require('./routes/employeeRoutes');
 
 
 const app = express();
 app.use(bodyParser.json());
 
+// Mount the employeeRoutes middleware
+app.use('/employees', employeeRoutes);
 
 const PORT = process.env.PORT || 3002; // Use the provided PORT environment variable or default to 3002
 
